f605f7556a8adb7d9804ac37c53e3ca2e203e1cc
verdecit <tedverdecia@gmail.com>
2023-10-09 23:05:27 -0400

    polished up the ui added some buttons and a scrollable timer selector, putting the live pop-up timer on back log to figure out later. Now moving on to saving user ulrs into a persistent local storage that can be accessed even after closing the browser. should be fully done within the next few days

--- background.js
+++ background.js
@@ -1,15 +1,71 @@
-
 // this function is listening in on the status of the timer while the tabs are active and when it "hears" that the timer has a status of "expired" it will change its color
+// const testing = async () => {
+//     try {
+//         // this is storing in local storage
+//         await chrome.storage.local.set({ test: 'this is a test ' }, (() => {
+//             console.log('value is set');
+//         }));
+        
+//         // this is retrieving in local storage
+//         const bs = await chrome.storage.local.get(['test'])
+
+//         // this is removing in local storage
+//         chrome.storage.local.remove(['test'], (() => {
+//             console.log('value '+ bs.test +' is removed');
+//         }));
+
+
+//     } catch (error) {
+//         console.error();
+//     }
+// }
+// testing();
+const checkStorage = async () => {
+    try {
+        const isAlive = await chrome.storage.local.get(['urls'])
+
+        if (isAlive === undefined || isAlive == {}){
+            await chrome.storage.local.set({ urls: [] }, (() => {
+                console.log("local url storage space is allocated");
+            }));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+checkStorage();
+
+try {
+    chrome.runtime.onMessage.addListener(async (message) => {
+        // await chrome.storage.local.get()
+    })
+} catch (error) {
+    console.error();
+}
+
+
+
+// retrieving a message saying open tab true to see if we can open a tab with the chrome api
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.openTab){
+        chrome.tabs.create({
+            url: 'https://www.youtube.com/'
+        })
+    }
+})
+
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
-        chrome.storage.local.set({ timerRunning: false });
+        chrome.storage.session.set({ timerRunning: false });
         chrome.action.setBadgeBackgroundColor({ color: [190, 190, 190, 230] });
     }
         
 });
 
 chrome.runtime.onMessage.addListener((message) => {
-    chrome.storage.local.set({ timerValue: message.timerform });
+    chrome.storage.session.set({ timerValue: message.timerform });
 })
 
 
@@ -17,11 +73,11 @@ chrome.runtime.onMessage.addListener((message) => {
 chrome.runtime.onMessage.addListener(async function(request) {
     if (request.startTimer) {
         
-        chrome.storage.local.set({ timerRunning: true });
+        chrome.storage.session.set({ timerRunning: true });
         chrome.action.setBadgeBackgroundColor({ color: [74, 0, 72, 39]});
         
         // using promises to get the timer value from our front end
-        const res = await chrome.storage.local.get(["timerValue"]);
+        const res = await chrome.storage.session.get(["timerValue"]);
 
         var timer = parseInt(res.timerValue) * 60;
         
@@ -32,7 +88,7 @@ chrome.runtime.onMessage.addListener(async function(request) {
                 if (!request.startTimer){
                     clearInterval(intervalId);
                     
-                    chrome.storage.local.set({ timerRunning: false});
+                    chrome.storage.session.set({ timerRunning: false});
                     chrome.action.setBadgeText({ text: ''});
                 }
             })
@@ -41,8 +97,19 @@ chrome.runtime.onMessage.addListener(async function(request) {
             var minutes = Math.floor(timer/60);
             var seconds = timer % 60;
             var text = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-            chrome.runtime.sendMessage({ updateTimer: true, time: text });
 
+            // for now keep this commented out
+
+            // const finalTest = async () => {
+            //     try {
+            //         await chrome.runtime.sendMessage({ updateTimer: true, time: text });
+                    
+            //     } catch (error) {
+            //         console.log("hate this -> " + error);
+            //     }
+            // }
+            
+            // finalTest();
             
             
             chrome.action.setBadgeText({ text: text });
@@ -54,20 +121,21 @@ chrome.runtime.onMessage.addListener(async function(request) {
                     'width': 250,
                     'height': 250
                 });
-                chrome.storage.local.set({ timerRunning: false});
+                chrome.storage.session.set({ timerRunning: false});
                 chrome.action.setBadgeText({ text: ''});
                 chrome.action.setBadgeBackgroundColor({ color: [190, 190, 190, 230] });
-                chrome.runtime.sendMessage({ updateTimer: false, time: '0:00' });
+                // this is tied to above bug same shit
+                // chrome.runtime.sendMessage({ updateTimer: false, time: '0:00' });
             }
-            
             
             
         }, 1000 );
     } 
     
     else if (request.restartTimer){
-        chrome.storage.local.set({ timerRunning: false });
-        chrome.runtime.sendMessage({ startTimer: true });
+        chrome.storage.session.set({ timerRunning: false });
+        
+        // chrome.runtime.sendMessage({ startTimer: true });
     } 
 
 });
--- main.js
+++ main.js
@@ -1,4 +1,51 @@
 document.addEventListener('DOMContentLoaded',function() {
+
+
+    const minutesElement = document.getElementById('minutes');
+    
+
+    let minutes = 0;
+
+    // Function to update the timer display
+    function updateTimerDisplay() {
+        minutesElement.textContent = minutes.toString().padStart(2, '0');
+    }
+
+    // Function to handle scrolling up (increase time)
+    function scrollUp() {
+        if (minutes < 59) {
+            minutes++;
+        } else {
+            minutes = 0;
+        }
+        updateTimerDisplay();
+    }
+
+    // Function to handle scrolling down (decrease time)
+    function scrollDown() {
+        if (minutes > 0) {
+            minutes--;
+        } else {
+            minutes = 59;
+        }
+        updateTimerDisplay();
+    }
+
+    // Event listeners for scroll events
+    window.addEventListener('wheel', (event) => {
+        if (event.deltaY < 0) {
+            // Scrolling up
+            scrollUp();
+        } else {
+            // Scrolling down
+            scrollDown();
+        }
+    });
+
+    // Initialize the timer display
+    updateTimerDisplay();
+
+
     document.getElementById('startTimer').addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, () => {
             chrome.runtime.sendMessage({ startTimer: true });
@@ -9,19 +56,28 @@ document.addEventListener('DOMContentLoaded',function() {
     document.getElementById('stopTimer').addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, () => {
             chrome.runtime.sendMessage({ startTimer: false });
-            window.close();
+            
         })
     });
 
     // grabbing changes from the timer form and sending a message over to through our runtime interface
     document.getElementById('timerform').addEventListener('submit', (event) => {
         event.preventDefault();
-        const timevalue = document.getElementById('time').value;
-        chrome.runtime.sendMessage({ 'timerform': timevalue });
+        // const timevalue = document.getElementById('time').value;
+        chrome.runtime.sendMessage({ timerform: minutes });
         
     });
 
-    // our countdown timer that shows up in the front end of the extension
+    // getting a saved website from the user to pass as a url to the tab create function
+    document.getElementById('toOpen').addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const user_url = document.getElementById('tabAddress').value;
+        chrome.runtime.sendMessage({ 'user_url': user_url})
+    })
+
+    // our countdown timer that shows up in the front end of the extension but for now were going to comment this out unitl
+    // we can fix the dumbass bug
         
     chrome.runtime.onMessage.addListener((req) => {
         if (req.updateTimer){
@@ -29,6 +85,14 @@ document.addEventListener('DOMContentLoaded',function() {
         }
         
     })    
+
+    // grabbing the id of our study button to see if we can trigger a test opening of a tab that we will manually input
+
+    document.getElementById('start').addEventListener('click', (event) => {
+        event.preventDefault();
+        chrome.runtime.sendMessage({ openTab: true })
+    })
+
 
 
 });
